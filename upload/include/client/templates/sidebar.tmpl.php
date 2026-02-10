fa50cfccb6e843f849d6b80a70cccdf0200e9054
root <srvrcps@gmail.com>
2026-02-10 14:17:52 +0700

    Initial commit: osTicket v1.17.2

---
+++ upload/include/client/templates/sidebar.tmpl.php
@@ -0,0 +1,26 @@
+        <div class="content"><?php
+    if ($cfg->isKnowledgebaseEnabled()
+        && ($faqs = FAQ::getFeatured()->select_related('category')->limit(5))
+        && $faqs->all()) { ?>
+            <section><div class="header"><?php echo __('Featured Questions'); ?></div>
+<?php   foreach ($faqs as $F) { ?>
+            <div><a href="<?php echo ROOT_PATH; ?>kb/faq.php?id=<?php
+                echo urlencode($F->getId());
+                ?>"><?php echo $F->getLocalQuestion(); ?></a></div>
+<?php   } ?>
+            </section>
+<?php
+    }
+    $resources = Page::getActivePages()->filter(array('type'=>'other'));
+    if ($resources->all()) { ?>
+            <section><div class="header"><?php echo __('Other Resources'); ?></div>
+<?php   foreach ($resources as $page) { ?>
+            <div><a href="<?php echo ROOT_PATH; ?>pages/<?php echo $page->getNameAsSlug();
+            ?>"><?php echo $page->getLocalName(); ?></a></div>
+<?php   } ?>
+            </section>
+<?php
+    }
+        ?></div>
+    </div>
+
